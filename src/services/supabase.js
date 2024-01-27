f49f655f926335e4197616976b698e33fc5d28de
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-27 08:17:51 +0530

    connect to supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://qwavsosofpfxvxzitssh.supabase.co'
+const supabaseKey = 
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3YXZzb3NvZnBmeHZ4eml0c3NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYyODcyMzUsImV4cCI6MjAyMTg2MzIzNX0.5v5pI4oQw2Q9ZAyqXyufv3iczA1N8aa07uj9eT7vQpA"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
