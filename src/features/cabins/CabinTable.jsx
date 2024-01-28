e94a0f106d8f7ffe7c781cb3b6a6cc43b0c338b5
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-28 21:05:52 +0530

    add cliet side filter on cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,12 +4,24 @@ import Table from "../../ui/Table";
 
 import { useCabins } from "./useCabins";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const {isLoading, cabins} = useCabins();
+  const [searchParams] = useSearchParams()
+
+  const filterValue = searchParams.get("discount") || "all";
   
   if(isLoading) return <Spinner/>
-  
+
+  let filterCabins;
+  if(filterValue === 'all') filterCabins = cabins;
+  if(filterValue === 'no-discount')
+  filterCabins = cabins.filter((cabin)=>cabin.discount === 0);
+
+  if(filterValue === 'with-discount')
+  filterCabins = cabins.filter((cabin)=>cabin.discount >0 );
+
   return (
     <Menus>
     <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -21,7 +33,7 @@ function CabinTable() {
     <div>Discount</div>
     <div></div>
     </Table.Header>
-    <Table.Body data ={cabins} render={(cabin=>
+    <Table.Body data ={filterCabins} render={(cabin=>
       <CabinRow cabin={cabin}key={cabin.id}/>)} 
       />
       </Table>
