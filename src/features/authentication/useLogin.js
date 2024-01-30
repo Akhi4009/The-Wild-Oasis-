ab269716c329c1e7266ae8f17fc5c6420fe080b4
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-30 09:17:51 +0530

    fix login bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -4,11 +4,14 @@ import toast from "react-hot-toast";
 import {login as loginApi} from "../../services/apiAuth"
 
 export function useLogin(){
-    
+    const queryClient = useQueryClient();
+
     const navigate = useNavigate();
     const {mutate:login,isLoading} = useMutation({
         mutationFn:({email,password})=>loginApi({email,password}),
         onSuccess:(user)=>{
+            console.log(user)
+            queryClient.setQueryData(['user'],user.user)
             navigate("/dashboard",{replace:true});
             toast.success("Login sucess");
         },
