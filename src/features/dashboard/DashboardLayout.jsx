cb4f55ef9067710817d989a67ae1526f91d4c73f
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-30 22:54:01 +0530

    dashboard stastics and chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,42 @@
 import styled from "styled-components";
+import Spinner from "../../ui/Spinner";
+import React from 'react'
+import { useRecentBooking } from "./useRecentBooking";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
+function DashboardLayout() {
+  const {bookings, isLoading, numDays} = useRecentBooking();
+  const {isLoading:isLoading1, stays,confirmedStays} = useRecentStays();
+  const {cabins, isLoading:isLoading2} = useCabins()
+
+  if(isLoading || isLoading1 || isLoading2) return <Spinner/>
+ 
+  const cabinCount = cabins?.length
+
+  return (
+    <StyledDashboardLayout>
+    <Stats bookings={bookings} 
+      confirmedStays={confirmedStays}
+      numDays={numDays}
+      cabinCount={cabinCount}
+      />
+    <div>Today's activity</div>
+    <div>Chart stay duration</div>
+    <SalesChart 
+      bookings={bookings} 
+      numDays={numDays}/>
+    </StyledDashboardLayout>
+    )
+}
+
+export default DashboardLayout
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 import DashboardBox from "./DashboardBox";
+import Heading from "../../ui/Heading";
+import {useDarkMode} from "../../context/DarkModeContext";
+import {AreaChart,Area, CartesianGrid, Tooltip, XAxis, YAxis, ResponsiveContainer} from "recharts";
+import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -42,9 +46,28 @@ const fakeData = [
   { label: "Feb 05", totalSales: 1400, extrasSales: 600 },
   { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
 ];
+    
+function SalesChart({bookings, numDays}) {
+      const {isDarkMode} = useDarkMode()
 
-const isDarkMode = true;
-const colors = isDarkMode
+    const allDates =  eachDayOfInterval({
+        start:subDays(new Date(),numDays - 1),
+        end:new Date(),
+      });
+
+      const data = allDates.map(date=>{
+        return {
+          label:format(date,'MMM dd'),
+          totalSales:bookings.filter(booking=> isSameDay
+            (date, new Date(booking.created_at))).reduce((acc,cur)=>
+            acc + cur.totalPrice,0),
+            extrasSales:bookings.filter(booking=> isSameDay
+              (date, new Date(booking.created_at))).reduce((acc,cur)=>
+              acc + cur.extrasPrice,0),
+        }
+      })
+
+      const colors = isDarkMode
   ? {
       totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
       extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
@@ -57,3 +80,44 @@ const colors = isDarkMode
       text: "#374151",
       background: "#fff",
     };
+      
+    return (
+       <StyledSalesChart>
+       <Heading as="h2"> Sales </Heading>
+       <ResponsiveContainer height={300} width="100%">
+       <AreaChart data={data}>
+       <XAxis dataKey='label' 
+        tick={{fill:colors.text}}
+        tickLine={{stroke:colors.text}}
+        />
+       <YAxis unit='$'
+       tick={{fill:colors.text}}
+       tickLine={{stroke:colors.text}}
+       />
+       <CartesianGrid strokeDasharray="4"/>
+       <Tooltip contentStyle={{background:colors.background}}/>
+       <Area 
+        dataKey='totalSales' 
+        type="monotone"
+        stroke={colors.totalSales.stroke}
+        fill={colors.totalSales.fill}
+        strokeWidth={2}
+        name="Total sales"
+        unit="$"
+        />
+        <Area 
+        dataKey='extrasSales' 
+        type="monotone"
+        stroke={colors.extrasSales.stroke}
+        fill={colors.extrasSales.fill}
+        strokeWidth={2}
+        name="Extras sales"
+        unit="$"
+        />
+       </AreaChart>
+       </ResponsiveContainer>
+       </StyledSalesChart>
+      )
+    }
+    
+    export default SalesChart
